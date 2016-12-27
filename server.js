4799784455b9a5909193db42d543c9304410100f
JTAO <jerky168@163.com>
2016-12-27 17:42:53 +0800

    add https support

--- server.js
+++ server.js
@@ -1,21 +1,52 @@
 var restify = require('restify');
 var plugins = require('restify-plugins');
+var fs = require('fs');
 
-
-const server = restify.createServer({
+var http_options = {
   name: 'Electromble@xiaoan',
   version: '1.0.0'
+};
+
+var keys_dir = './';
+
+const http_server = restify.createServer(http_options);
+
+var https_options = {
+  name: 'Electromble@xiaoan',
+  key: fs.readFileSync(keys_dir + 'privatekey.key'), //on current folder
+  certificate: fs.readFileSync(keys_dir + 'certificate.cert')
+};
+
+const https_server = restify.createServer(https_options);
+
+// Put any routing, response, etc. logic here. This allows us to define these functions
+// only once, and it will be re-used on both the HTTP and HTTPs servers
+var setup_server = function (app) {
+  function respond(req, res, next) {
+    res.send('I see you ' + req.params.name);
+  }
+
+  // Middleware
+  app.use(plugins.acceptParser(app.acceptable));
+  app.use(plugins.queryParser());
+  app.use(plugins.bodyParser());
+
+  // Routes
+  app.get('/v1/history/:imei', function (req, res, next) {
+    console.log('GET %s', req.url);
+    res.send(req.params);
+    return next();
+  });
+}
+
+// Now, setup both servers in one step
+setup_server(http_server);
+setup_server(https_server);
+
+http_server.listen(80, function () {
+  console.log('%s listening at %s', http_server.name, http_server.url);
 });
-server.use(plugins.acceptParser(server.acceptable));
-server.use(plugins.queryParser());
-server.use(plugins.bodyParser());
 
-
-server.get('/v1/history/:imei', function (req, res, next) {
-  res.send(req.params);
-  return next();
-});
-
-server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
+https_server.listen(443, function () {
+  console.log('%s listening at %s', https_server.name, https_server.url);
 });
