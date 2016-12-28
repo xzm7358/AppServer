5b52f928209d2d99e35d3677725619647f4a94ee
JTAO <jerky168@163.com>
2016-12-28 13:05:00 +0800

    get the parameter from request

--- history.js
+++ history.js
@@ -5,6 +5,14 @@ var history = exports;
 
 history.get = function(req, res, next) {
     console.log('GET %s', req.url);
+
+    var imei = req.params.imei;
+    var start = req.query.start;
+    var end = req.query.end;
+    
+    console.log(imei);
+    //TODO: get the data from the DB
+
     res.send(req.params);
     return next();
 }
