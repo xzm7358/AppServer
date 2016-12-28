ceb39392c843288b61a85a1de6078b88ef4dafb4
piggysticker <641777437@qq.com>
2016-12-28 15:39:22 +0800

    history模块 OK

--- history.js
+++ history.js
@@ -1,6 +1,7 @@
 /**
  * Created by jk on 2016-12-28.
  */
+var mysql = require('mysql');
 var history = exports;
 
 history.get = function(req, res, next) {
@@ -9,10 +10,42 @@ history.get = function(req, res, next) {
     var imei = req.params.imei;
     var start = req.query.start;
     var end = req.query.end;
-    
     console.log(imei);
-    //TODO: get the data from the DB
+    console.log(start);
+    console.log(end);
+    res.setHeader('Content-Type', 'text/plain');
+    if(!imei)
+    {
+        res.send(JSON.stringify({"code":101}));
+        return next();
+    }
+    if(start && !end)
+    {
+        end =  start + 86400 - (start % 86400);
+        selectsql = 'SELECT * FROM ' + 'gps_' + imei + ' WHERE '+ 'timestamp' + ' BETWEEN ' + start + ' AND ' + end;
+    }
+    if(!start)
+    {
+        selectsql = 'SELECT * FROM ' + 'gps_' + imei + ' order by timestamp desc limit 1 ';
+    }
 
-    res.send(req.params);
+    var connnection = mysql.createConnection({
+        host : 'test.xiaoan110.com',
+        user : 'eelink',
+        password: 'eelink',
+        database: 'gps',
+    });
+    connnection.connect();
+    connnection.query(selectsql, function (starterr, startresult){
+        if (starterr)
+        {
+            console.log('[SELECT ERROR - ', starterr.message);
+            var rsp = JSON.stringify({"code":101});
+            res.send(rsp);
+        }
+        var rsp = JSON.stringify({"gps":startresult});
+        res.send(rsp);
+    });
+    connnection.end();
     return next();
 }
