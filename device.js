eafbb6aee8695ae3fd7c279e8fbed59b79026b4a
zzs <zzsshun13@gmail.com>
2017-01-06 11:26:53 +0800

    device接口完成

--- device.js
+++ device.js
@@ -4,26 +4,45 @@
 
 var device = exports;
 
-var request = require('request');
+var http = require('http');
+var querystring = require('querystring');
 
 device.post = function (req, res, next) {
     console.log(req.body);
     console.log('POST '+ req.url);
     res.contentType = 'json';
-    var transurl =  'http://' + req.headers.host + ':8081/v1/device';//
-    console.log('transurl %s',transurl);
-    var pipe = req.pipe(request.post(transurl, {form:req.body}));//
-    console.log('pipe: %s', pipe);
-    // var response = [];
-    // pipe.on('data', function (chunk) {
-    //     response.push(chunk);
-    // })
-    pipe.on('end',function () {
-        var res2 = pipe.response;
-        console.log('res2 %s',res2);
-        res.send(res2);
+    // var transurl =  'http://' + req.headers.host + ':8081/v1/device';//
+    var transdata = JSON.stringify(req.body);
+    console.log('transdata:', transdata);
+    var options = {
+        host: req.headers.host,
+        port: 8081,
+        path: "/v1/device",
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+            "Content-Length": transdata.length
+        }
+    };
+    var requset = http.request(options, function (response) {
+       console.log('status:', response.statusCode);
+       console.log('headers:', response.headers);
+       if (response.statusCode === 200) {
+           var body = "";
+           response.on('data', function (data) {
+               console.log(data);
+               body += data;
+           }).on('end', function () {
+               res.send(200, JSON.parse(body));
+           })
+       }
+       else {
+           res.send(500,"error");
+       }
+
     });
+    requset.write(transdata + "\n");
+    requset.end();
 
     return next();
-
 };
