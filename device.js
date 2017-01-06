4bc1de18aeb2792fa2da1df5b93aee0150d76207
zzs <zzsshun13@gmail.com>
2017-01-06 13:41:52 +0800

    根据测试结果修改了device接口，已完成

--- device.js
+++ device.js
@@ -7,41 +7,47 @@ var device = exports;
 var http = require('http');
 
 device.post = function (req, res, next) {
-    console.log(req.body);
     console.log('POST '+ req.url);
     res.contentType = 'json';
-    // var transurl =  'http://' + req.headers.host + ':8081/v1/device';//
-    var transdata = JSON.stringify(req.body);
-    console.log('transdata:', transdata);
-    var options = {
-        host: req.headers.host,
-        port: 8081,
-        path: "/v1/device",
-        method: "POST",
-        headers: {
-            "Content-Type": 'application/json',
-            "Content-Length": transdata.length
-        }
-    };
-    var requset = http.request(options, function (response) {
-       console.log('status:', response.statusCode);
-       console.log('headers:', response.headers);
-       if (response.statusCode === 200) {
-           var body = "";
-           response.on('data', function (data) {
-               console.log(data);
-               body += data;
-           }).on('end', function () {
-               res.send(200, JSON.parse(body));
-           })
-       }
-       else {
-           res.send(500,"error");
-       }
-
-    });
-    requset.write(transdata);
-    requset.end();
+    if ( !req.body )
+    {
+        console.log('error');
+        res.send({code:100});
+    }
+    else
+    {
+        var transdata = JSON.stringify(req.body);
+        console.log('transdata:', transdata);
+        var options = {
+            host: "test.xiaoan110.com",
+            port: 8082,
+            path: "/v1/device",
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json',
+                "Content-Length": transdata.length
+            }
+        };
+        var requset = http.request(options, function (response) {
+            console.log('status:', response.statusCode);
+            console.log('headers:', response.headers);
+            if (response.statusCode === 200) {
+                var body = "";
+                response.on('data', function (data) {
+                    body += data;
+                    console.log(data);
+                }).on('end', function () {
+                    console.log('json:', JSON.parse(body));
+                    res.send(200, JSON.parse(body));
+                })
+            }
+            else {
+                res.send({code:100});
+            }
+        });
+        requset.write(transdata);
+        requset.end();
+    }
 
     return next();
 };
